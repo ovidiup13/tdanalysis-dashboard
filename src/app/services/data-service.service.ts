de605c31169aa556023a5da87081b43ad322ebd6
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-12 14:19:56 +0000

    Extend data service. Explore repositories

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -7,6 +7,8 @@ import { Repository } from "../models/repository.interface";
 
 // env
 import { environment } from "../../environments/environment.dev";
+import { Issue } from "../models/issue.interface";
+import { Commit } from "../models/commit.interface";
 
 @Injectable()
 export class DataService {
@@ -14,7 +16,60 @@ export class DataService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Returns metadata about the repository.
+   * @param id of the repository
+   */
   getRepository(id: string): Observable<Repository> {
     return this.http.get<Repository>(`${DataService.API}/repos/${id}`);
+  }
+
+  /**
+   * Returns all issues associated with the repository.
+   * @param id of the repository
+   */
+  getIssues(id: string): Observable<Issue[]> {
+    return this.http.get<Issue[]>(`${DataService.API}/repos/${id}/issues`);
+  }
+
+  /**
+   * Returns metadata about the issue.
+   * @param repoId repository ID
+   * @param issueId issue ID
+   */
+  getIssue(repoId: string, issueId: string): Observable<Issue> {
+    return this.http.get<Issue>(
+      `${DataService.API}/repos/${repoId}/issues/${issueId}`
+    );
+  }
+
+  /**
+   * Returns all commits from the repository, ordered by timestamp.
+   * @param id of the repository
+   */
+  getCommits(id: string): Observable<Commit[]> {
+    return this.http.get<Commit[]>(`${DataService.API}/repos/${id}/commits`);
+  }
+
+  /**
+   * Returns metadata about a specific commit (revision) of the repository.
+   * @param repoId repository ID
+   * @param sha commit SHA
+   */
+  getCommit(repoId: string, sha: string): Observable<Commit> {
+    return this.http.get<Commit>(
+      `${DataService.API}/repos/${repoId}/commits/${sha}`
+    );
+  }
+
+  /**
+   * Returns all commits from the repository referencing the specified issue.
+   * @param repoId id of the repository
+   * @param issueId id of the ticket from issue tracker
+   */
+  getCommitsByIssue(repoId: string, issueId: string) {
+    return this.http.get<Commit[]>(
+      `${DataService.API}/repos/${repoId}/issues/${issueId}/commits`
+    );
   }
 }
