c64e80c76c9b1e06104d3423a7230fd3181d8bd7
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-16 14:05:08 +0000

    Add repository viewer container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,47 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
 // components
 import { AppComponent } from "./containers/app.component";
+import { NotFoundComponent } from "./not-found.component";
+import { SimpleChartComponent } from "./components/simple-chart/simple-chart.component";
+import { RepositoryDetailComponent } from "./components/repository-detail/repository-detail.component";
 
 // services
 import { DataService } from "./services/data-service.service";
-import { SimpleChartComponent } from "./components/simple-chart/simple-chart.component";
+
+// containers
 import { RepositoryDashboardComponent } from "./containers/repository-dashboard/repository-dashboard.component";
-import { RepositoryDetailComponent } from "./components/repository-detail/repository-detail.component";
+import { RepositoryViewerComponent } from "./containers/repository-viewer/repository-viewer.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: AppComponent,
+    pathMatch: "full" // match empty string
+  },
+  {
+    path: "repositories/:id",
+    component: RepositoryViewerComponent
+  },
+  {
+    path: "**", // any routes that do not exist in the application
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
     SimpleChartComponent,
     RepositoryDashboardComponent,
-    RepositoryDetailComponent
+    RepositoryDetailComponent,
+    RepositoryViewerComponent
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
---
+++ src/app/containers/repository-viewer/repository-viewer.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { DataService } from "../../services/data-service.service";
+import { Observable } from "rxjs/Observable";
+import { Stats } from "../../models/stats.interface";
+import { switchMap } from "rxjs/operators";
+
+@Component({
+  selector: "app-repository-viewer",
+  templateUrl: "./repository-viewer.component.html",
+  styleUrls: ["./repository-viewer.component.css"]
+})
+export class RepositoryViewerComponent implements OnInit {
+  workEffortData: Stats[];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+
+  ngOnInit() {
+    this.route.params
+      .pipe(
+        switchMap((data: Params) => {
+          return this.dataService.getSimpleStats(data.id);
+        })
+      )
+      .subscribe((data: Stats[]) => {
+        this.workEffortData = data;
+      });
+  }
+}
